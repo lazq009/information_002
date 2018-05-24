950c7b21ee893d17dd2715d5173622540d2f19cb
lxf <lxf@163.com>
2018-05-24 21:28:14 +0800

    CSRFProtect 的集成配置

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
-
+from flask_wtf.csrf import CSRFProtect
 
 
 class Config(object):
     ''' 配置文件的加载'''
+    # 设置秘钥
+    SECRET_KEY = 'UsJI6EtahFAkzf+BXqK9/Xn1qaisKAf2w4YIx1ZJIcrP3/Ue1BjyQeeA6bTfH6q0'
     # 开启debug 调试模式
     DEBUG = True
     # 配置mysql 数据库  实际开发时写真实的ip
@@ -24,6 +26,9 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # 创建redis数据库的对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启csrf 保护
+CSRFProtect(app)
+
 @app.route('/')
 def index():
     # 测试下 redis
