c0ef8930b003553b61e1cbc8464781050a395921
lxf <lxf@163.com>
2018-06-01 09:56:42 +0800

    查询新闻用户详情页 和 点击量排行

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,12 +1,35 @@
 # 新闻详情   收藏   评论
 from . import news_blue
-from flask import render_template
+from flask import render_template, session, current_app
+from info.models import User, News
+from info import constants
 
 
 @news_blue.route('/detail/<int:news_id>')
 def news_detail(news_id):
-    '''新闻详情'''
+    '''新闻详情
+    1 获取登陆用户信息
+    '''
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        # 如果有user_id  说明登陆成功  取出User 模型类对象信息
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
 
+    # 2 主页点击排行 查询新闻数据 根据clicks 点击量属性 实现倒叙
+    news_clicks = []
+    try:
+        news_clicks = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
 
+    context = {
+        'user': user,
+        'news_clicks': news_clicks
+
+    }
     # 渲染新闻详情页
-    return render_template('news/detail.html')
+    return render_template('news/detail.html', context=context)
